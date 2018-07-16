10c91d8a95496fd8ea8d1207e61c66ff86dfc73d
husein zolkepli <husein.zol05@gmail.com>
2018-07-16 10:12:26 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = "malaya",
     packages = ["malaya"],
-    version = "0.7",
+    version = "0.0.8",
     description = "nltk like for Bahasa Malaysia",
     author = "huseinzol05",
     author_email = "husein.zol05@gmail.com",
