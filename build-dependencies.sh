c5aa780334c2c14ccfbd3473d9565047a91ddbfa
husein zolkepli <husein.zol05@gmail.com>
2018-09-12 01:35:42 +0800

    add build yml

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -1,3 +1,4 @@
 pip3 install scikit-learn==0.19.1 requests fuzzywuzzy tqdm nltk unidecode numpy scipy python-levenshtein tensorflow==1.5 coverage
 python3 -m nltk.downloader punkt
+git clone https://github.com/DevconX/Malaya && cd Malaya
 python3 setup.py install
