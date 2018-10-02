0fdc3b694affeed92048073c51efb8a151681965
khursani8 <khursani8@gmail.com>
2018-10-03 00:50:39 +0900

    pip setup

--- setup.py
+++ setup.py
@@ -2,31 +2,33 @@ import os
 from distutils.core import setup
 from distutils import dir_util
 from distutils import sysconfig
+import setuptools
+
 
 __packagename__ = 'malaya'
 
-setup(
-    name = __packagename__,
-    packages = [__packagename__],
-    version = '0.5',
-    description = 'Natural-Language-Toolkit for bahasa Malaysia, powered by Deep Learning.',
-    author = 'huseinzol05',
-    author_email = 'husein.zol05@gmail.com',
-    url = 'https://github.com/DevconX/Malaya',
-    download_url = 'https://github.com/DevconX/Malaya/archive/master.zip',
-    keywords = ['nlp','bm'],
-    classifiers = [
+setuptools.setup(
+    name=__packagename__,
+    packages=setuptools.find_packages(),
+    # packages=[__packagename__],
+    version='0.5',
+    description='Natural-Language-Toolkit for bahasa Malaysia, powered by Deep Learning.',
+    author='huseinzol05',
+    author_email='husein.zol05@gmail.com',
+    url='https://github.com/DevconX/Malaya',
+    download_url='https://github.com/DevconX/Malaya/archive/master.zip',
+    keywords=['nlp', 'bm'],
+    classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Development Status :: 0.5 - Beta',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Linguistic',
-        ],
-    long_description = '''\
+    ],
+    long_description='''\
 Universal character encoding detector
 -------------------------------------
 
