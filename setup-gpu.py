c8bc6db7be0c6418cc762dba1c677186f0f70e3c
husein zolkepli <husein.zol05@gmail.com>
2018-12-01 07:53:37 +0800

    bumpt to 0.7.5.1

--- setup-gpu.py
+++ setup-gpu.py
@@ -30,7 +30,7 @@ __packagename__ = 'malaya-gpu'
 setuptools.setup(
     name = __packagename__,
     packages = setuptools.find_packages(),
-    version = '0.7.5',
+    version = '0.7.5.1',
     description = 'Natural-Language-Toolkit for bahasa Malaysia, powered by Deep Learning. GPU Version',
     author = 'huseinzol05',
     author_email = 'husein.zol05@gmail.com',
