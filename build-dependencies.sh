f1b2ec8e4efbd200f5e256a48c3294db04d2ac21
husein zolkepli <husein.zol05@gmail.com>
2018-12-11 18:13:24 +0800

    push build travis

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -1,5 +1,5 @@
 pip3 install xgboost==0.80 tqdm nltk unidecode numpy scipy python-levenshtein tensorflow
 pip3 install sklearn_crfsuite sklearn scikit-learn==0.19.1 requests fuzzywuzzy
-pip3 install pytest pytest-cov coveralls coverage pandas toolz
+pip3 install pandas toolz
 pip3 install PySastrawi
-python3 -m nltk.downloader punkt
+python3 setup.py install
