9985b88ca0f55218324bf223aca8a664c9de1d94
husein zolkepli <husein.zol05@gmail.com>
2019-01-02 12:51:07 +0800

    added anaconda build

--- build-package.sh
+++ build-package.sh
@@ -1,7 +1,9 @@
 rm dist/*
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
+anaconda upload dist/malaya-*.tar.gz
 
 rm dist/*
 python3 setup-gpu.py sdist bdist_wheel
 twine upload dist/*
+anaconda upload dist/malaya-gpu-*.tar.gz
