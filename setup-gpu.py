c63278f3844fe7dfa1d076e4bcefd59aafa028dc
husein zolkepli <husein.zol05@gmail.com>
2019-12-08 21:17:30 +0800

    bump setup version to 3.1.2

--- setup-gpu.py
+++ setup-gpu.py
@@ -6,7 +6,7 @@ __packagename__ = 'malaya-gpu'
 setuptools.setup(
     name = __packagename__,
     packages = setuptools.find_packages(),
-    version = '3.1.1',
+    version = '3.1.2',
     python_requires = '>=3.6.*',
     description = 'Natural-Language-Toolkit for bahasa Malaysia, powered by Deep Learning Tensorflow. GPU Version',
     author = 'huseinzol05',
