01ac026f9a5cb5ae3b15708250d81effa357d0e4
husein zolkepli <husein.zol05@gmail.com>
2021-07-10 13:58:06 +0800

    remove gpu version

--- malaya/utils/__init__.py
+++ malaya/utils/__init__.py
@@ -1,8 +1,6 @@
 from malaya_boilerplate.utils import (
     available_device,
     available_gpu,
-    gpu_available,
-    is_gpu_version,
     print_cache,
     delete_cache,
     delete_all_cache,
