736af2d63282a6758e994f23c6f399405e1e60cd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-05 20:24:02 +0000

    Adding tarfile member sanitization to extractall()

--- malaya/transformers/albert/__init__.py
+++ malaya/transformers/albert/__init__.py
@@ -294,7 +294,26 @@ def load(model: str = 'albert', **kwargs):
         import tarfile
 
         with tarfile.open(path['model']) as tar:
-            tar.extractall(path=PATH_ALBERT[model]['path'])
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=PATH_ALBERT[model]["path"])
 
     bert_checkpoint = os.path.join(PATH_ALBERT[model]['directory'], 'model.ckpt')
     vocab_model = os.path.join(PATH_ALBERT[model]['directory'], 'sp10m.cased.v10.model')
--- malaya/transformers/bert/__init__.py
+++ malaya/transformers/bert/__init__.py
@@ -286,7 +286,26 @@ def load(model: str = 'base', **kwargs):
         import tarfile
 
         with tarfile.open(path['model']) as tar:
-            tar.extractall(path=PATH_BERT[model]['path'])
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=PATH_BERT[model]["path"])
 
     bert_checkpoint = os.path.join(PATH_BERT[model]['directory'], 'model.ckpt')
     vocab_model = os.path.join(PATH_BERT[model]['directory'], 'sp10m.cased.bert.model')
