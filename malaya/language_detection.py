8b9fb62c5cde3cc12a24de714ac3fa4985c8f8aa
huseinzol05 <husein.zol05@gmail.com>
2023-06-16 00:14:02 +0800

    improve language detection

--- malaya/language_detection.py
+++ malaya/language_detection.py
@@ -19,6 +19,62 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+metrics = {
+    'deep-model': """
+              precision    recall  f1-score   support
+
+         eng    0.96760   0.97401   0.97080    553739
+         ind    0.97635   0.96131   0.96877    576059
+       malay    0.96985   0.98498   0.97736   1800649
+    manglish    0.98036   0.96569   0.97297    181442
+       other    0.99641   0.99627   0.99634   1428083
+       rojak    0.94221   0.84302   0.88986    189678
+
+    accuracy                        0.97779   4729650
+   macro avg    0.97213   0.95421   0.96268   4729650
+weighted avg    0.97769   0.97779   0.97760   4729650
+""",
+    'mesolitica/fasttext-language-detection-v1': """
+              precision    recall  f1-score   support
+
+         eng    0.94014   0.96750   0.95362    553739
+         ind    0.97290   0.97316   0.97303    576059
+       malay    0.98674   0.95262   0.96938   1800649
+    manglish    0.96595   0.98417   0.97498    181442
+       other    0.98454   0.99698   0.99072   1428083
+       rojak    0.81149   0.91650   0.86080    189678
+
+    accuracy                        0.97002   4729650
+   macro avg    0.94363   0.96515   0.95375   4729650
+weighted avg    0.97111   0.97002   0.97028   4729650
+""",
+    'mesolitica/fasttext-language-detection-ms-id': """
+                        precision    recall  f1-score   support
+
+           local-malay    0.95063   0.93858   0.94457    199961
+                 other    0.97145   0.98889   0.98009    125920
+socialmedia-indonesian    0.97923   0.96303   0.97106    213486
+   standard-indonesian    0.99119   0.99610   0.99364    149055
+        standard-malay    0.93743   0.95669   0.94696    149336
+
+              accuracy                        0.96584    837758
+             macro avg    0.96599   0.96866   0.96727    837758
+          weighted avg    0.96591   0.96584   0.96582    837758
+""",
+    'mesolitica/fasttext-language-detection-en': """
+                  precision    recall  f1-score   support
+
+   local-english    0.88991   0.89457   0.89223    149823
+        manglish    0.98619   0.98479   0.98549    149535
+           other    0.99439   0.99268   0.99354    140651
+standard-english    0.89162   0.88967   0.89064    150703
+
+        accuracy                        0.93952    590712
+       macro avg    0.94053   0.94043   0.94047    590712
+    weighted avg    0.93960   0.93952   0.93955    590712
+""",
+}
+
 lang_labels_v1 = {
     0: 'eng',
     1: 'ind',
@@ -41,8 +97,25 @@ lang_labels_v2 = {
     9: 'other',
 }
 
+lang_labels_ms_id = {
+    0: 'standard-indonesian',
+    1: 'socialmedia-indonesian',
+    2: 'standard-malay',
+    3: 'local-malay',
+    4: 'other',
+}
+
+lang_labels_en = {
+    0: 'standard-english',
+    1: 'local-english',
+    2: 'manglish',
+    3: 'other',
+}
+
 label_v1 = list(lang_labels_v1.values())
 label_v2 = list(lang_labels_v2.values())
+label_ms_id = list(lang_labels_ms_id.values())
+label_en = list(lang_labels_en.values())
 
 _fasttext_availability = {
     'mesolitica/fasttext-language-detection-v1': {
@@ -52,22 +125,22 @@ _fasttext_availability = {
         'Label': lang_labels_v1,
     },
     'mesolitica/fasttext-language-detection-v2': {
-        'Size (MB)': 1330,
-        'Quantized Size (MB)': 127,
+        'Size (MB)': 1840,
+        'Quantized Size (MB)': 227,
         'dim': 16,
         'Label': lang_labels_v2,
     },
-    'mesolitica/fasttext-language-detection-v3': {
-        'Size (MB)': 1660,
-        'Quantized Size (MB)': 135,
-        'dim': 24,
-        'Label': lang_labels_v2,
+    'mesolitica/fasttext-language-detection-ms-id': {
+        'Size (MB)': 537,
+        'Quantized Size (MB)': 62.5,
+        'dim': 16,
+        'Label': label_ms_id,
     },
-    'mesolitica/fasttext-language-detection-auto': {
-        'Size (MB)': 5150,
-        'Quantized Size (MB)': 162,
-        'dim': 101,
-        'Label': lang_labels_v2,
+    'mesolitica/fasttext-language-detection-en': {
+        'Size (MB)': 383,
+        'Quantized Size (MB)': 42.3,
+        'dim': 16,
+        'Label': label_ms_id,
     }
 }
 
