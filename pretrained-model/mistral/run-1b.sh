7e8ed0f69ba5764a3472a476ea9228697635e2cc
huseinzol05 <husein.zol05@gmail.com>
2023-11-27 23:02:15 +0800

    improve mistral

--- pretrained-model/mistral/run-1b.sh
+++ pretrained-model/mistral/run-1b.sh
@@ -1,8 +1,8 @@
 WANDB_PROJECT=pretrain-mistral-1.1b python3 train.py \
---model_name_or_path huseinzol05/dummy-mistral-1b \
---storage_directory "/home/ubuntu" \
+--model_name_or_path huseinzol05/dummy-mistral-1.1b \
 --share_directory "/home/ubuntu/share" \
 --torch_dtype "bfloat16" \
 --train_file "/home/ubuntu/share/combine-all" \
 --block_size 4096 \
---num_workers 20
+--num_workers 20 \
+--checkpoint_steps 100
