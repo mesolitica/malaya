7165e93ee06ab803649a089f5ee8b3ecb857b51d
aisyahrzk <aisyahrazak171@gmail.com>
2023-11-27 22:27:28 +0800

    added yi

---
+++ session/yi/run.sh
@@ -0,0 +1,21 @@
+rm -rf /dev/shm/*
+WANDB_PROJECT="finetune-yi-6b" \
+~/.local/bin/torchrun --nproc_per_node 4 \
+-m train \
+--model_name_or_path 01-ai/Yi-6B \
+--per_device_train_batch_size 15 \
+--gradient_accumulation_steps 1 \
+--output_dir finetune-yi-6b \
+--bf16 \
+--do_train \
+--do_eval false \
+--num_train_epochs 2 \
+--train_file "combine-all" \
+--logging_steps 1 \
+--learning_rate 2e-5 \
+--block_size 4096 \
+--save_steps 200 \
+--save_total_limit 3 \
+--gradient_checkpointing true \
+--deepspeed ds_config_zero3.json \
+--log_level "info"
