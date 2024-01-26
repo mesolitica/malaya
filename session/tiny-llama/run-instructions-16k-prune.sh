fb5b214b96e467cd9c58384ad38169f6158202ea
huseinzol05 <husein.zol05@gmail.com>
2024-01-26 13:59:32 +0800

    improve mallam

--- session/tiny-llama/run-instructions-16k-prune.sh
+++ session/tiny-llama/run-instructions-16k-prune.sh
@@ -1,14 +1,14 @@
-WANDB_PROJECT=fpf-tinyllama-1.1b-hf-instructions-16k-v2 \
+WANDB_PROJECT=fpf-prune-tinyllama-1.1b-hf-instructions-16k \
 deepspeed run-instruction-packing.py \
 --deepspeed ds_config_zero3.json \
---model_name_or_path mesolitica/tinyllama-1.1b-4096-fpf \
---per_device_train_batch_size 8 \
+--model_name_or_path huseinzol05/570M-tinyllama \
+--per_device_train_batch_size 16 \
 --gradient_accumulation_steps 1 \
---output_dir fpf-1.1b-instructions-16k-call-v2 \
+--output_dir fpf-prune-1.1b-instructions-16k \
 --bf16 \
 --do_train \
 --do_eval false \
---num_train_epochs 5 \
+--num_train_epochs 1 \
 --train_file 'mosaic-chat-instruction-v5-tinyllama-16k' \
 --logging_steps 1 \
 --learning_rate 2e-5 \
