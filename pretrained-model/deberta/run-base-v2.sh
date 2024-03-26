40e9f26da8abbbf8d15e8f2c321558946cef77e9
aisyahrzk <aisyahrazak171@gmail.com>
2024-03-26 19:11:48 +0800

    add deberta-v2

---
+++ pretrained-model/deberta/run-base-v2.sh
@@ -0,0 +1,21 @@
+rm -rf /dev/shm/*
+WANDB_PROJECT=deberta-base \
+~/.local/bin/torchrun --nproc_per_node 4 \
+-m run-mlm \
+--tokenizer_name malaysia-ai/bpe-tokenizer \
+--config_name microsoft/deberta-v3-base \
+--per_device_train_batch_size 75 \
+--do_train \
+--max_seq_len 512 \
+--output_dir debertav2-base-malaysian-v2 \
+--mlm_probability 0.15 \
+--train_file "mosaic-combine-512" \
+--logging_steps="1" \
+--save_steps="1000" \
+--bf16 \
+--learning_rate 2e-4 \
+--warmup_steps 10000 \
+--do_train \
+--do_eval false \
+--num_train_epochs 10 \
+--save_total_limit 2
